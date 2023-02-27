64fe3115ce14fd83d027f87b613b70ee1d03561f
Kirilenko <>
2023-02-28 00:02:10 +0400

    feat: replace widgets.wbs.host to git-submodule

--- remote/widgets.wbs.host/src/shared/ui/index.tsx
+++
@@ -1,2 +0,0 @@
-// @ts-ignore
-export { Header } from 'remote-shared-ui/ui'
--- remote/widgets.wbs.host/src/widgets/wbs/wbs.tsx
+++
@@ -1,6 +0,0 @@
-import { Header } from '@shared/ui'
-
-export default () => <>
-  <Header title='shared.ui.header in widgets.wbs' />
-  <h2>remote.widgets.wbs.host</h2>
-</>
