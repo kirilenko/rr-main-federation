bb95a7588fcc091c47992df707852824856c7680
Kirilenko <>
2023-02-28 12:17:35 +0400

    feat: add linters

--- main.host/src/app/app.tsx
+++ main.host/src/app/app.tsx
@@ -1,10 +1,22 @@
-import Wbs from '@widgets/wbs'
-import { Header } from '@shared/ui'
+import { FC } from 'react'
+import { Global } from '@emotion/react'
 
-export default () => (
-	<>
-		<h1>main.host</h1>
-		<Header title='shared.ui.header in main.host' />
-		<Wbs />
-	</>
+import { Header } from '@shared/ui'
+import Wbs from '@widgets/wbs'
+
+import StyledApp from './styles/app.style'
+import GlobalStyles from './styles/global.style'
+
+const App: FC = () => (
+  <>
+    <Global styles={GlobalStyles} />
+    <StyledApp>
+      <h1>main.host</h1>
+      <Header title="shared.ui.header in main.host" />
+      <Wbs />
+    </StyledApp>
+    <h1>main.host</h1>
+  </>
 )
+
+export default App
--- main.host/src/widgets/wbs/index.tsx
+++ main.host/src/widgets/wbs/index.tsx
@@ -1,3 +1,4 @@
 // @ts-ignore
 import Wbs from 'remote-widgets-wbs/wbs'
+
 export default Wbs // don't use here: export { default } from 'remote-widgets-wbs/wbs'
