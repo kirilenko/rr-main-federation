df8f2bc088d1f4dd25dc4a4961b2d198d3a7e69f
Kirilenko <>
2023-03-01 15:47:35 +0400

    chore: fix format-config

--- main.host/vite.config.ts
+++ main.host/vite.config.ts
@@ -1,33 +1,33 @@
-import { defineConfig } from 'vite'
 import federation from '@originjs/vite-plugin-federation'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => ({
-	plugins: [
-		react(),
-		federation({
-			name: 'main',
-			remotes: {
-				'remote-widgets-wbs': 'http://localhost:3001/assets/remote-entry.js',
-				'remote-shared-ui': 'http://localhost:3002/assets/remote-entry.js',
-			},
-			shared: ['react','react-dom'],
-		}),
-		// tsconfigPaths() from vite-tsconfig-paths does not work with federation
-	],
-	build: {
-		modulePreload: false,
-		target: 'esnext',
-		minify: mode === 'production',
-		cssCodeSplit: false
-	},
-	resolve: {
-		alias: {
-			'@app': '/src/app',
-			'@entities': '/src/entities',
-			'@pages': '/src/pages',
-			'@shared': '/src/shared',
-		  '@widgets': '/src/widgets',
-		},
-	},
-}));
+  build: {
+    cssCodeSplit: false,
+    minify: mode === 'production',
+    modulePreload: false,
+    target: 'esnext',
+  },
+  plugins: [
+    react(),
+    federation({
+      name: 'main',
+      remotes: {
+        'remote-shared-ui': 'http://localhost:3002/assets/remote-entry.js',
+        'remote-widgets-wbs': 'http://localhost:3001/assets/remote-entry.js',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+    // tsconfigPaths() from vite-tsconfig-paths does not work with federation
+  ],
+  resolve: {
+    alias: {
+      '@app': '/src/app',
+      '@entities': '/src/entities',
+      '@pages': '/src/pages',
+      '@shared': '/src/shared',
+      '@widgets': '/src/widgets',
+    },
+  },
+}))
